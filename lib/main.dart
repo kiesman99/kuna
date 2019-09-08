b0c15885626ff951589b633a12eb159ea61c5177
Justin <justin.vietz@gmx.de>
2019-09-08 21:06:13 +0200

    Added logic to open HiveBoxes (DB)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:kuna/pages/intro/intro_page.dart';
 import 'package:kuna/pages/my_home_page.dart';
 import 'package:kuna/provider/calculator_provider.dart';
 import 'package:kuna/provider/page_provider.dart';
 import 'package:kuna/provider/shared_pref_provider.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:provider/provider.dart';
 
@@ -13,6 +16,15 @@ class MyApp extends StatelessWidget {
   SharedPrefProvider _sharedPrefProvider;
   PageProvider _pageProvider;
   CalculatorProvider _calculatorProvider;
+
+  Future _openBoxes() async{
+    var dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+
+    return Future.wait([
+      Hive.openBox("settings")
+    ]);
+  }
 
   /// This function will instantiate the needed Provider and emit them to
   /// the CalculatorProvider instance that will be created, so that the other
@@ -33,9 +45,81 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(builder: (_) => _pageProvider),
         ChangeNotifierProvider(builder: (_) => _calculatorProvider)
       ],
-      child: MaterialApp(
-        home: IntroPage(),
+      child: FutureBuilder(
+        future: _openBoxes(),
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            if(snapshot.error != null){
+              return _Error(error: snapshot.error.toString());
+            }
+
+            return WatchBoxBuilder(
+              box: Hive.box("settings"),
+              builder: (context, box){
+                return MaterialApp(
+                  theme: ThemeData(
+                    primaryColor: Color(box.get("primaryColor", defaultValue: Colors.blue.value))
+                  ),
+                  home: _Main(box: box),
+                );
+              },
+            );
+          }
+
+          return _Loading();
+        },
       ),
     );
   }
 }
+
+class _Main extends StatelessWidget {
+
+  final Box box;
+
+  _Main({this.box});
+
+  @override
+  Widget build(BuildContext context) {
+    return box.get("wasOpenedBefore", defaultValue: false) ? MyHomePage() : IntroPage();
+  }
+}
+
+class _Error extends StatelessWidget {
+
+  final String error;
+
+  _Error({this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Text("Etwas ist schiefgelaufen..."),
+              SizedBox(height: 50.0),
+              Text(error)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+
