71c1865856a8f298b11cd0c82bdc0f39a44562a3
Justin <justin.vietz@gmx.de>
2019-09-12 08:54:49 +0200

    Added some more values to expose

--- lib/provider/shared_pref_provider.dart
+++ lib/provider/shared_pref_provider.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-class SharedPrefProvider with ChangeNotifier {
-  double course = 7.33;
+class SettingsProvider with ChangeNotifier {
+
+  Box get box => Hive.box("settings");
+
+  double get course => box.get("course", defaultValue: 0.0);
+  set course(double course) => box.put("course", course);
+
+  int get primaryColor => box.get("primaryColor", defaultValue: Colors.blue.value);
+  set primaryColor(int color) => box.put("primaryColor", color);
+
+  bool get courseSet => box.get("courseSet", defaultValue: false);
+  set courseSet(bool set) => box.put("courseSet", set);
+
+  bool get tutorialWatched => box.get("tutorialWatched", defaultValue: false);
+  set tutorialWatched(bool watched) => box.put("tutorialWatched", watched);
+
 }
